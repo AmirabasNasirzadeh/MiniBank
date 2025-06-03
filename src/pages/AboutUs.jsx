3c95c4641111f691dc39b2d2790a4964f36bbad2
Amirabas Nasirzadeh <13.amirabas.nasirzadeh.86@gmail.com>
2025-06-03 15:39:35 +0330

    About us & Why Mini bank pages UI

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,24 @@
 function AboutUs() {
-  return <div>About us...</div>;
+  return (
+    <div class="content__section">
+      <div class="content__image--wrapper">
+        <img
+          src="AboutUs.jpg"
+          alt="Banking illustration"
+          class="content__image"
+        />
+      </div>
+      <div class="content__text--group">
+        <p class="content__description">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit,
+          magni reiciendis commodi eum nulla sint sit deserunt! Tempore
+          architecto, inventore aspernatur sequi alias doloremque quod qui
+          impedit placeat atque at. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default AboutUs;
--- src/pages/WhyMiniBank.jsx
+++ src/pages/WhyMiniBank.jsx
@@ -1,5 +1,23 @@
 function WhyMiniBank() {
-  return <div>Why MiniBank...</div>;
+  return (
+    <div class="content__section">
+      <div class="content__text--group">
+        <p class="content__description">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex ipsa
+          itaque quas rerum nam iste, cum similique. Beatae quibusdam vel
+          laudantium mollitia similique cumque a iste alias quo, magni nihil?
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        </p>
+      </div>
+      <div class="content__image--wrapper">
+        <img
+          src="WhyMiniBank.jpg"
+          alt="Banking illustration"
+          class="content__image"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default WhyMiniBank;
