3fde0b45d57b02c25797d1d7f228e179c8f199cc
Amirabas Nasirzadeh <13.amirabas.nasirzadeh.86@gmail.com>
2025-06-09 14:46:45 +0330

    Transfer money page UI

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,65 +5,133 @@ import { signup, login, logout } from "../features/accounts/accountsSlice";
 
 function Form() {
   const dispatch = useDispatch();
+  const { isLoggedin } = useSelector((state) => state.accounts);
   const [hasAccount, setHasAccount] = useState(true);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   return (
     <div class="form__container">
-      <h2 class="form__header">{hasAccount ? "Login" : "Sign up"}</h2>
-      <form
-        class="form__content"
-        onSubmit={(e) => {
-          e.preventDefault();
-          hasAccount
-            ? dispatch(login(username, password))
-            : dispatch(signup(username, password));
-        }}
-      >
+      <h2 class="form__header">Hello, Amir</h2>
+      <h2 class="form__header">Balance: $5,000</h2>
+
+      <div class="form__row">
         <div class="form__group">
-          <label for="username" class="form__label">
-            Username
+          <label for="amount" class="form__label">
+            Amount
+          </label>
+          <input
+            type="number"
+            id="amount"
+            class="form__input"
+            placeholder="$0.00"
+            min={0}
+            max={1000}
+          />
+        </div>
+
+        <div class="form__group">
+          <label for="account" class="form__label">
+            Destination
           </label>
           <input
             type="text"
-            id="username"
+            id="account"
             class="form__input"
-            placeholder={
-              hasAccount ? "Enter your username" : "Enter a username"
-            }
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Account name"
           />
         </div>
+        <button
+          type="button"
+          class="form__btn form__btn--transfer form__btn--money"
+        >
+          Transfer
+        </button>
+      </div>
+
+      <div class="form__row">
         <div class="form__group">
-          <label for="password" class="form__label">
-            Password
+          <label for="loan-amount" class="form__label">
+            Amount
           </label>
           <input
-            type="password"
-            id="password"
+            type="number"
+            id="loan-amount"
             class="form__input"
-            placeholder={
-              hasAccount ? "Enter your password" : "Enter a password"
-            }
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            placeholder="$0.00"
+            min={0}
+            max={1000}
           />
         </div>
-        <button type="submit" class="form__btn">
-          {hasAccount ? "Login" : "Sign up"}
+        <button
+          type="button"
+          class="form__btn form__btn--loan form__btn--money"
+        >
+          Request Loan
         </button>
-      </form>
-      <p
-        className="form__situation"
-        onClick={() => setHasAccount((situation) => !situation)}
-      >
-        {hasAccount
-          ? "You don't have an account? Sign up."
-          : "You have an account? Login."}
-      </p>
+        <button type="button" class="form__btn form__btn--pay form__btn--money">
+          Pay Loan ($0)
+        </button>
+      </div>
+
+      <p class="form__situation">Logout from your account</p>
+      <br />
+      <p class="form__situation">Delete your account</p>
     </div>
+    // <div class="form__container">
+    //   <h2 class="form__header">{hasAccount ? "Login" : "Sign up"}</h2>
+    //   <form
+    //     class="form__content"
+    //     onSubmit={(e) => {
+    //       e.preventDefault();
+    //       hasAccount
+    //         ? dispatch(login(username, password))
+    //         : dispatch(signup(username, password));
+    //     }}
+    //   >
+    //     <div class="form__group">
+    //       <label for="username" class="form__label">
+    //         Username
+    //       </label>
+    //       <input
+    //         type="text"
+    //         id="username"
+    //         class="form__input"
+    //         placeholder={
+    //           hasAccount ? "Enter your username" : "Enter a username"
+    //         }
+    //         value={username}
+    //         onChange={(e) => setUsername(e.target.value)}
+    //       />
+    //     </div>
+    //     <div class="form__group">
+    //       <label for="password" class="form__label">
+    //         Password
+    //       </label>
+    //       <input
+    //         type="password"
+    //         id="password"
+    //         class="form__input"
+    //         placeholder={
+    //           hasAccount ? "Enter your password" : "Enter a password"
+    //         }
+    //         value={password}
+    //         onChange={(e) => setPassword(e.target.value)}
+    //       />
+    //     </div>
+    //     <button type="submit" class="form__btn">
+    //       {hasAccount ? "Login" : "Sign up"}
+    //     </button>
+    //   </form>
+    //   <p
+    //     className="form__situation"
+    //     onClick={() => setHasAccount((situation) => !situation)}
+    //   >
+    //     {hasAccount
+    //       ? "You don't have an account? Sign up."
+    //       : "You have an account? Login."}
+    //   </p>
+    // </div>
   );
 }
 
